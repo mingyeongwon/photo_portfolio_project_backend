cab0fb2f26f16bf02d6f35dd255368e5fac0f1c2
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-09-29 20:44:52 +0900

    Category,Project 엔티티 생성

---
+++ src/main/java/com/example/portfolio/model/Category.java
@@ -0,0 +1,62 @@
+package com.example.portfolio.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    // 단일 Project를 반환하므로 메소드 이름 수정
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public Category() {
+        super();
+    }
+
+    public Category(Long id, String name, Project project) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.project = project;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + "]";
+    }
+}
