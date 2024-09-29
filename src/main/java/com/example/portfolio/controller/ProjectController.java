2edf162a67b65caa078f834ef50ec0ee6f309a26
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-09-29 21:05:29 +0900

    프로젝트 컨트롤러에 카테고리 전체 데이터 부분 추가

--- src/main/java/com/example/portfolio/controller/ProjectController.java
+++ src/main/java/com/example/portfolio/controller/ProjectController.java
@@ -1,11 +1,24 @@
 package com.example.portfolio.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.portfolio.model.Category;
+import com.example.portfolio.service.CategoryService;
 
 @RestController
 @RequestMapping("/api")
 public class ProjectController {
 
-
+	@Autowired
+	private CategoryService categoryService;
+	
+	@GetMapping("/category")
+	public List<Category> getAllCategories() {
+		return categoryService.getAllCategories();
+	}
 }
