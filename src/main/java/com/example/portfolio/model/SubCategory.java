dc8333a530e53bfb2466ebff6a585c77cbff3286
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-03 12:50:54 +0900

    Dto 추가, 엔티티 수정

--- src/main/java/com/example/portfolio/model/SubCategory.java
+++ src/main/java/com/example/portfolio/model/SubCategory.java
@@ -17,7 +17,7 @@ public class SubCategory {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 
 	private String name;
 
@@ -33,11 +33,11 @@ public class SubCategory {
 		super();
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
