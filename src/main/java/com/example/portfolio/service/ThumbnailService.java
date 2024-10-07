e78c0dd4c7599dc9e140c0343f8311f7fb38cbbd
shyeseon <gptjs1222@gmail.com>
2024-10-07 11:41:59 +0900

    gcs 이미지 삭제하기 수정

--- src/main/java/com/example/portfolio/service/ThumbnailService.java
+++ src/main/java/com/example/portfolio/service/ThumbnailService.java
@@ -102,6 +102,7 @@ public class ThumbnailService {
                 .build()
                 .getService();
         
+        
 		Thumbnail thumbnail = thumbnailRepository.findById(id)
 				.orElseThrow(() -> new RuntimeException("Thumbnail not found"));
 		String url= thumbnail.getImageUrl();
