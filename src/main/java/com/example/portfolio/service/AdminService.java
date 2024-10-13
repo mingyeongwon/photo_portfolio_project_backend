831280b4f21acdbeff1b7990cac732aef2286f91
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-13 12:43:15 +0900

    필드 주입 방식 -> 생성자 주입 방식으로 변경

--- src/main/java/com/example/portfolio/service/AdminService.java
+++ src/main/java/com/example/portfolio/service/AdminService.java
@@ -8,16 +8,16 @@ import com.example.portfolio.repository.AdminRepository;
 
 @Service
 public class AdminService {
-	
-    private AdminRepository adminRepository;
-    private PasswordEncoder passwordEncoder; 
 
-    public AdminService(AdminRepository adminRepository, PasswordEncoder passwordEncoder) {
-        this.adminRepository = adminRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-	
-    // 회원가입
+	private final AdminRepository adminRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	public AdminService(AdminRepository adminRepository, PasswordEncoder passwordEncoder) {
+		this.adminRepository = adminRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	// 회원가입
 	public void signUpAdmin(Admin admin) {
 		// 비밀번호 암호화
 		admin.setPassword(passwordEncoder.encode(admin.getPassword()));
