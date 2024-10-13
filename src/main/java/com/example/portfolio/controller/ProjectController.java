03f155750234033abd227c5b5c7ebd13ba46028f
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-13 12:32:46 +0900

    트렌젝션 어노테이션 제거 및 사용하지 않는 메소드들 주석 처리 완료, 컨트롤러에서 호출되는 서비스 코드 개선

--- src/main/java/com/example/portfolio/controller/ProjectController.java
+++ src/main/java/com/example/portfolio/controller/ProjectController.java
@@ -1,5 +1,6 @@
 package com.example.portfolio.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,14 +19,9 @@ import com.example.portfolio.dto.ProjectUpdateDto;
 import com.example.portfolio.dto.SubCategoryDto;
 import com.example.portfolio.model.Admin;
 import com.example.portfolio.model.Category;
-import com.example.portfolio.model.Project;
 import com.example.portfolio.service.AdminService;
 import com.example.portfolio.service.CategoryService;
-import com.example.portfolio.service.PhotoService;
 import com.example.portfolio.service.ProjectService;
-import com.example.portfolio.service.ThumbnailService;
-
-import jakarta.transaction.Transactional;
 
 @RestController
 @RequestMapping("/api")
@@ -34,76 +30,35 @@ public class ProjectController {
 	private final CategoryService categoryService;
 	private final ProjectService projectService;
 	private final AdminService adminService;
-	private final PhotoService photoService;
-	private final ThumbnailService thumbnailService;
 
 	// 생성자 주입
 	public ProjectController(CategoryService categoryService, ProjectService projectService,
-			ThumbnailService thumbnailService, AdminService adminService, PhotoService photoService) {
+			AdminService adminService) {
 		this.categoryService = categoryService;
 		this.projectService = projectService;
-		this.thumbnailService = thumbnailService;
 		this.adminService = adminService;
-		this.photoService = photoService;
-	}
-	
-	// 프로젝트 생성 
-	@PostMapping("/create/project")
-	public void createProject(@ModelAttribute ProjectCreateDto projectCreateDto) {
-		// 프로젝트 생성
-		Project savedProject = projectService.createProject(projectCreateDto);
-		// 썸네일 생성
-		thumbnailService.createThumbnail(projectCreateDto, savedProject);
-		// 상세 사진 생성
-		photoService.createPhotos(projectCreateDto, savedProject);
 	}
 
-//	// 프로젝트 수정
-//	// param id 값 어떻게 할지 생각해야함
-//	@Transactional
-//	@PutMapping("/update/project/{id}")
-//	public void updateProject(@ModelAttribute ProjectUpdateDto projectUpdateDto,
-//			@ModelAttribute ThumbnailCreateDto thumbnailCreateDto, @PathVariable("id") Long id) throws IOException {
-//		// 프로젝트 ID 확인
-//		if (id == null) {
-//			throw new IllegalArgumentException("Project ID가 null입니다.");
-//		} else {
-//			projectUpdateDto.setId(id);
-//		}
-//
-//		// 썸네일 파일이 존재하는지 체크
-//		if (thumbnailCreateDto.getMultipartFile() != null && !thumbnailCreateDto.getMultipartFile().isEmpty()
-//				&& projectUpdateDto.getId() != null) {
-//			// 프로젝트 업데이트
-//			Project updatedProject = projectService.updateProject(projectUpdateDto);
-//
-//			// 썸네일 업데이트
-//			thumbnailService.updateThumbnail(thumbnailCreateDto, id, updatedProject);
-//
-//			// 썸네일이 수정되면 상세 사진도 업데이트
-//			photoService.updatePhotos(projectUpdateDto);
-//		} else {
-//			// 썸네일이 없으면 예외를 던지거나 업데이트를 막음
-//			throw new IllegalArgumentException("썸네일 파일이 없습니다. 썸네일이 없으면 프로젝트와 사진을 수정할 수 없습니다.");
-//		}
-//	}
-	
-	// 삭제
-	@Transactional
+	// 프로젝트 생성
+	@PostMapping("/create/project")
+	public void createProject(@ModelAttribute ProjectCreateDto projectCreateDto) {
+		projectService.createProject(projectCreateDto);
+	}
+
+	// 프로젝트 수정
+	@PutMapping("/update/project/{id}")
+	public void updateProject(@ModelAttribute ProjectUpdateDto projectUpdateDto, @PathVariable("id") Long id)
+			throws IOException {
+		// 프로젝트 ID 설정
+		projectUpdateDto.setId(id);
+		// 프로젝트 업데이트 서비스 호출
+		projectService.updateProject(projectUpdateDto);
+	}
+
+	// 프로젝트 삭제
 	@DeleteMapping("/delete/project/{id}")
 	public void deleteProjecct(@PathVariable("id") Long id) {
-//		try {
-			thumbnailService.deleteThumbnail(id);
-			photoService.deletePhotosByProjectId(id);
-			projectService.deleteProject(id);
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} 
-//			catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		projectService.deleteProject(id);
 	}
 
 	// 아이디 만들기
@@ -112,31 +67,31 @@ public class ProjectController {
 		adminService.signUpAdmin(admin);
 		return "회원가입 성공";
 	}
-    
-    // 카테고리 전체 목록 가져오기
-    @GetMapping("/categories")
-    public List<CategoryDto> getAllCategories() {
-        return categoryService.getAllCategories();
-    }
-    
-    // 카테고리 생성
+
+	// 카테고리 전체 목록 가져오기
+	@GetMapping("/categories")
+	public List<CategoryDto> getAllCategories() {
+		return categoryService.getAllCategories();
+	}
+
+	// 카테고리 생성
 //  @Secured("ROLE_ADMIN")
-    @PostMapping("/categories")
-    public void createCategories(@RequestBody List<CategoryDto> categoryDtos) {
-        categoryService.createCategories(categoryDtos);
-    }
-    
-    // 카테고리 수정
-    @PutMapping("/categories")
-    public void updateCategories(@RequestBody List<CategoryDto> categoryDtos) {
-        categoryService.updateCategories(categoryDtos);
-    }
-    
-    // 카테고리 삭제
-    @DeleteMapping("/categories")
-    public void deleteCategories(@RequestBody List<CategoryDto> categoryDtos) {
-        categoryService.deleteCategories(categoryDtos);
-    }
+	@PostMapping("/categories")
+	public void createCategories(@RequestBody List<CategoryDto> categoryDtos) {
+		categoryService.createCategories(categoryDtos);
+	}
+
+	// 카테고리 수정
+	@PutMapping("/categories")
+	public void updateCategories(@RequestBody List<CategoryDto> categoryDtos) {
+		categoryService.updateCategories(categoryDtos);
+	}
+
+	// 카테고리 삭제
+	@DeleteMapping("/categories")
+	public void deleteCategories(@RequestBody List<CategoryDto> categoryDtos) {
+		categoryService.deleteCategories(categoryDtos);
+	}
 
 	@GetMapping("/category")
 	public List<Category> getCategory() {
@@ -180,23 +135,23 @@ public class ProjectController {
 //		thumbnailService.deleteThumbnail(id);
 //	}
 
-	// 프로젝트 저장
-	@PostMapping("/project")
-	public void saveProject(ProjectCreateDto projectCreateDto) {
-		projectService.createProject(projectCreateDto);
-	}
+//	// 프로젝트 저장
+//	@PostMapping("/project")
+//	public void saveProject(ProjectCreateDto projectCreateDto) {
+//		projectService.createProject(projectCreateDto);
+//	}
 
-	// 프로젝트 업데이트
-	@PutMapping("/project")
-	public void updateProject(ProjectUpdateDto projectUpdateDto) {
-		projectService.updateProject(projectUpdateDto);
+//	// 프로젝트 업데이트
+//	@PutMapping("/project")
+//	public void updateProject(ProjectUpdateDto projectUpdateDto) {
+//		projectService.updateProject(projectUpdateDto);
+//
+//	}
 
-	}
-
-	// 프로젝트 삭제
-	@DeleteMapping("/project/{id}")
-	public void deleteProject(@PathVariable("id") Long id) {
-		projectService.deleteProject(id);
-	}
+//	// 프로젝트 삭제
+//	@DeleteMapping("/project/{id}")
+//	public void deleteProject(@PathVariable("id") Long id) {
+//		projectService.deleteProject(id);
+//	}
 
 }
