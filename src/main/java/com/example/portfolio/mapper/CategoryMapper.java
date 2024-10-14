a79552139bc79c7dc12a69a769d260dfd3fe9611
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-14 21:24:13 +0900

    카테고리 메퍼 추가

--- src/main/java/com/example/portfolio/mapper/CategoryMapper.java
+++ src/main/java/com/example/portfolio/mapper/CategoryMapper.java
@@ -10,34 +10,12 @@ import com.example.portfolio.model.SubCategory;
 public class CategoryMapper {
 
 	// Category -> CategoryDto 변환
-	public static CategoryDto toDto(Category category) {
-		CategoryDto categoryDto = new CategoryDto();
-		categoryDto.setId(category.getId());
-		categoryDto.setName(category.getName());
-		categoryDto
-				.setSubCategories(category.getSubCategories().stream().map(CategoryMapper::toSubCategoryDto).toList());
-		return categoryDto;
-	}
+
 
 	// CategoryCreateDto -> Category 변환
-	public static Category toEntity(CategoryCreateDto categoryCreateDto) {
-		Category category = new Category();
-		category.setName(categoryCreateDto.getName());
-		return category;
-	}
 
 	// CategoryUpdateDto -> Category 변환 (업데이트)
-	public static void updateEntity(CategoryUpdateDto categoryUpdateDto, Category category) {
-		if (categoryUpdateDto.getName() != null) {
-			category.setName(categoryUpdateDto.getName());
-		}
-	}
-
+	
 	// SubCategory -> SubCategoryDto 변환
-	public static SubCategoryDto toSubCategoryDto(SubCategory subCategory) {
-		SubCategoryDto subCategoryDto = new SubCategoryDto();
-		subCategoryDto.setId(subCategory.getId());
-		subCategoryDto.setName(subCategory.getName());
-		return subCategoryDto;
-	}
+
 }
