697d04aa11e686c3425380004d99137bcd12a33a
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-16 13:04:30 +0900

    카테고리를 가지고 있는지 확인하는 서브카테고리 메소드

--- src/main/java/com/example/portfolio/repository/SubCategoryRepository.java
+++ src/main/java/com/example/portfolio/repository/SubCategoryRepository.java
@@ -2,12 +2,15 @@ package com.example.portfolio.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.portfolio.model.SubCategory;
 import java.util.List;
 
-
 @Repository
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
-	List<SubCategory> findByCategory_id(Long categoryId);
+
+    // 카테고리 ID로 서브카테고리 목록 조회
+    List<SubCategory> findByCategory_Id(Long categoryId);
+
+    // 카테고리 ID로 서브카테고리가 존재하는지 확인
+    boolean existsByCategory_Id(Long categoryId);
 }
