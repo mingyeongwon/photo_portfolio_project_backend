bade5e51c81c6c6caccde2f32325cd4fe842dc2a
leek94 <khwan929@naver.com>
2024-10-17 13:37:31 +0900

    레포지토리 unit 테스트 완료

---
+++ src/test/java/com/example/portfolio/controller/ProjectControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.portfolio.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
