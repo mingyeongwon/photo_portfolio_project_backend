536d0916a74ad34e09d0f1da55234f3033aee121
mingyeongwon <wonmingy@gmail.com>
2024-11-16 00:52:29 +0900

    feat: GenericJackson2JsonRedisSerializer 사용

---
+++ src/main/java/com/example/portfolio/config/CacheConfig.java
@@ -0,0 +1,25 @@
+package com.example.portfolio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class CacheConfig {
+	
+	@Bean
+	public RedisCacheManager redisCacheManager (RedisConnectionFactory redisConnectionFactory) {
+		RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+				.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+				.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+	
+		return RedisCacheManager.builder(redisConnectionFactory)
+				.cacheDefaults(cacheConfiguration)
+				.build();
+	}
+}
