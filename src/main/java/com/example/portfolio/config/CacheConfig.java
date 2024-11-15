39538671eb0e447cc38fce6a5f82194161d01fbf
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-11-16 01:38:47 +0900

    fix: 캐시 설정 백업

--- src/main/java/com/example/portfolio/config/CacheConfig.java
+++ src/main/java/com/example/portfolio/config/CacheConfig.java
@@ -10,37 +10,28 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class CacheConfig {
-
+	
 	@Bean
-	public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
-		// Custom ObjectMapper 생성
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.registerModule(new Jdk8Module()); // JDK 8 모듈
-		objectMapper.registerModule(new JavaTimeModule()); // Java Time 모듈
-		objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+	public RedisCacheManager redisCacheManager (RedisConnectionFactory redisConnectionFactory) {
+	    ObjectMapper objectMapper = new ObjectMapper();
+	    objectMapper.registerModule(new Jdk8Module()); // Optional: JDK 8 모듈
+	    objectMapper.registerModule(new JavaTimeModule()); // Optional: Java Time 모듈
+	    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
-		// activateDefaultTyping 설정 추가
-		objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, // 서브타입 유효성 검사
-				ObjectMapper.DefaultTyping.NON_FINAL // final이 아닌 모든 클래스에 타입 정보 저장
-		);
-
-		// GenericJackson2JsonRedisSerializer에 Custom ObjectMapper 적용
-		GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
-
-		// RedisCacheConfiguration 설정
+	    GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+		
 		RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-				.serializeKeysWith(
-						RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+				.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
 				.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
-
-		// RedisCacheManager 반환
-		return RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(cacheConfiguration).build();
+	
+		return RedisCacheManager.builder(redisConnectionFactory)
+				.cacheDefaults(cacheConfiguration)
+				.build();
 	}
 }
